f3aef2163c1ddeb58ff23cd01a33f8c08051efec
Shawn Wilson <swilson1337@gmail.com>
2016-10-28 23:23:31 -0400

    Update GridView.php

--- src/GridView.php
+++ src/GridView.php
@@ -6,9 +6,15 @@ class GridView extends \kartik\grid\GridView
 {
 	public $pjax = true;
 	
-	public $layout = "{items}\n<div class=\"pull-left\">{summary}</div><div class=\"pull-right\">{pager}</div>";
+	public $layout = "<div class=\"row\"><div class=\"col-sm-12\"><div class=\"pull-right\">{export}</div></div></div><br /><div class=\"row\"><div class=\"col-sm-12\">{items}</div></div><div class=\"row\"><div class=\"col-sm-6\">{summary}</div><div class=\"col-sm-6\"><div class=\"pull-right\">{pager}</div></div></div>";
 	
 	public $showFilters = true;
+	
+	public $export = [
+		'label' => 'Export',
+		'target' => '_self',
+		'showConfirmAlert' => false,
+	];
 	
 	public function init()
 	{
