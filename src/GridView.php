96c39879ab63817a398b2581c07b93260ea23c4a
Shawn Wilson <swilson1337@gmail.com>
2016-10-30 00:12:12 -0400

    Update GridView.php

--- src/GridView.php
+++ src/GridView.php
@@ -6,32 +6,45 @@ use yii\helpers\ArrayHelper;
 
 class GridView extends \kartik\grid\GridView
 {
-	public $pjax = true;
-	
-	public $showFilters = true;
-	
-	private $defaultPanelOptions = [
+	private static $defaultPanelOptions = [
 		'type' => GridView::TYPE_DEFAULT,
-		'after' => '{summary}',
+		'after' => false,
 		'heading' => false,
+		'footer' => '<div class="pull-right">{summary}</div>',
 	];
 	
-	private $defaultExportOptions = [
+	private static $defaultExportOptions = [
 		'label' => 'Export Data To File',
 		'target' => '_self',
 	];
 	
+	public $panelFooterTemplate = '<div class="pull-left kv-panel-pager">{pager}</div>{footer}<div class="clearfix"></div>';
+	
+	public $panelTemplate = '<div class="{prefix}{type}">{panelHeading}{panelBefore}<div class="panel-body">{items}</div>{panelAfter}{panelFooter}</div>';
+	
+	public $toggleData = false;
+	
+	public $pjax = true;
+	
+	public $showFilters = true;
+	
 	public function init()
 	{
-		parent::init();
+		if (is_array($this->export))
+		{
+			$this->export = ArrayHelper::merge(static::$defaultExportOptions, $this->export);
+		}
 		
-		$this->export = ArrayHelper::merge($this->defaultExportOptions, (is_array($this->export) ? $this->export : []));
-		
-		$this->panel = ArrayHelper::merge($this->defaultPanelOptions, (is_array($this->panel) ? $this->panel : []));
+		if (is_array($this->panel))
+		{
+			$this->panel = ArrayHelper::merge(static::$defaultPanelOptions, $this->panel);
+		}
 		
 		if (!$this->showFilters)
 		{
 			$this->filterRowOptions['style'] = 'display: none;';
 		}
+		
+		parent::init();
 	}
 }
