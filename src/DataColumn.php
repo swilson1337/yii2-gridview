c2641a8466b522c7d3c5938d0804a40d67f5de13
Shawn Wilson <swilson1337@gmail.com>
2016-11-01 09:52:24 -0400

    Added showing/hiding filter

--- src/DataColumn.php
+++ src/DataColumn.php
@@ -4,4 +4,15 @@ namespace swilson1337\grid;
 
 class DataColumn extends \kartik\grid\DataColumn
 {
+	public $showFilter = true;
+	
+	public function init()
+	{
+		if (!$this->showFilter)
+		{
+			$this->filterInputOptions['style'] = 'display: none;';
+		}
+		
+		parent::init();
+	}
 }
